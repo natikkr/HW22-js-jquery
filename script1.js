84663acb326a54e030a765b2de38a202c4859063
natikkr <82267830+natikkr@users.noreply.github.com>
2021-09-19 00:11:10 +0300

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,21 @@
+$('.test').css('color', 'red');
+
+$('h2.test').css('color', 'red');
+
+$('p + p').css('color', 'red');
+
+$('li + li.www').css('color', 'red');
+
+$('h2 i').css('color', 'red');
+
+$('h2 > i').css('color', 'red');
+
+$(':header').css('color', 'red');
+
+$('h2:not(.test)').css('color', 'red');
+
+$(':header:not(.test)').css('color', 'red');
+
+$(':header + :header').css('color', 'red');
+
+$(':header.test + :header.test').css('color', 'red');
